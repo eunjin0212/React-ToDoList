46f0f1434ee9fa680d2e058da27536402037bf16
eunjin0212 <eunjin9639@gmail.com>
2021-01-31 17:34:40 +0900

    Progress.tsx

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -2,35 +2,7 @@ import React from "react";
 import { useState } from "../context";
 import styled from "styled-components";
 
-const ProgressBar = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 20px;
-`;
-const Bar = styled.div`
-  display: flex;
-  align-items: center;
-  color: #ff80ab;
-  & > *:not(:last-child) {
-    margin-right: 20px;
-  }
-  span {
-    font-size: 20px;
-  }
-`;
-
-const GoalDiv = styled.div`
-  text-align: center;
-  margin: 10px;
-  font-size: 18px;
-  font-weight: 700;
-  text-transform: uppercase;
-  letter-spacing: 0.5px;
-`;
-
-// eslint-disable-next-line
-export default () => {
+const Progress = () => {
   const { toDos, completed } = useState();
   const percent = Math.floor(
     (completed.length / (toDos.length + completed.length)) * 100
@@ -66,3 +38,32 @@ export default () => {
     </>
   );
 };
+
+export default Progress;
+
+const ProgressBar = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+const Bar = styled.div`
+  display: flex;
+  align-items: center;
+  color: #ff80ab;
+  & > *:not(:last-child) {
+    margin-right: 20px;
+  }
+  span {
+    font-size: 20px;
+  }
+`;
+
+const GoalDiv = styled.div`
+  text-align: center;
+  margin: 10px;
+  font-size: 18px;
+  font-weight: 700;
+  text-transform: uppercase;
+  letter-spacing: 0.5px;
+`;
