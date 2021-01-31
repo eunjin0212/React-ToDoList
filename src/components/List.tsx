fb98771dcd6828242a816cb3954ec6d0a3b8ba63
eunjin0212 <eunjin9639@gmail.com>
2021-01-31 17:30:42 +0900

    List.tsx

---
+++ src/components/List.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+interface lists {
+  title: string;
+  children: any;
+}
+
+
+const List = ({ title, children }: lists) => (
+  <>
+    <Ul>
+      <H2>{title}</H2>
+      {children}
+    </Ul>
+  </>
+);
+
+export default List;
+
+const Ul = styled.ul`
+  margin-bottom: 20px;
+  padding: 0;
+`;
+const H2 = styled.h2`
+  margin: 0;
+  padding-bottom: 10px;
+`;
